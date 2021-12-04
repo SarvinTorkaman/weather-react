7b7a3364b4e529a90548d88935bd7ae2a0bac0e4
SarvinTorkaman <88194904+SarvinTorkaman@users.noreply.github.com>
2021-12-04 12:16:45 +0330

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -4,11 +4,5 @@ import Icons from "./WeatherIcon/index.js";
 export default function WeatherIcon(props) {
   let element = props.icon;
 
-  for (let i in Icons) {
-    console.log(`element of Icons is ${i}=${Icons[i]}`);
-  }
-
-  console.log(`Icons is ${Icons}`);
-
   return <img src={Icons[element]} alt={props.description} width={100} />;
 }
